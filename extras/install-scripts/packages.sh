ac59db56fd8c6da313de896f65902f820cb86429
J. Matt Peterson <jmatt@jmatt.org>
2011-11-29 14:28:18 -0700

    ATMO-2 Updates to packages.sh

--- extras/install-scripts/packages.sh
+++ extras/install-scripts/packages.sh
@@ -1,1 +1,16 @@
 #!/bin/sh
+
+if [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    DISTRO=$DISTRIB_ID
+elif [ -f /etc/debian_version ]; then
+    DISTRO=`cat /etc/lsb-release | cut -f 2 -d "=" | awk -F "\n" 'NR==1 {print $1}'`
+    # XXX or Ubuntu
+elif [ -f /etc/redhat-release ]; then
+    DISTRO=`cat /etc/redhat-release | cut -f 1 -d " "`
+    # XXX or CentOS or Fedora
+else
+    DISTRO=$(uname -s)
+fi
+
+echo "$DISTRO"
