11e2172d4a341efdac17454dd87f91a7b15df59f
esteve <esteve@iplantcollaborative.org>
2011-11-29 13:46:48 -0700

    ATMO-3 Added getopts functionality to atmo.sh

--- extras/install-scripts/atmo.sh
+++ extras/install-scripts/atmo.sh
@@ -1,1 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+################## Default Variables ######################
+VERBOSE="FALSE"
+INSTALL_DIR=`pwd`
+
+############### Command-Line Configuration ################
+while getopts vd: option
+do
+        case "${option}"
+        in
+                d) INSTALL_DIR=${OPTARG};;
+                v) VERBOSE="TRUE";;
+                \?) usage
+                    exit 1;; 
+        esac
+done
+
+echo "Script variables"
+echo $VERBOSE
+echo $INSTALL_DIR
