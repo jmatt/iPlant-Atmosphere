aa281405cd752113b1c80d1a5affc0963d3b6982
J. Matt Peterson <jmatt@jmatt.org>
2011-12-04 23:38:39 -0700

    [ATMO-2] packages.sh should be complete for centos6. Added emacs.sh. /selfish && emacs so power\!

--- extras/install-scripts/emacs.sh
+++ extras/install-scripts/emacs.sh
@@ -21,38 +21,30 @@ else
     # Linux Darwin
 fi
 
-PACKAGES=""
-
+############### Install Emacs ################
 if [ "$DISTRO" = "CentOS" ] || [ "$DISTRO" = "Fedora" ] ; then
     # Use yum
     COMMAND="yum"
     OPTIONS="-y"
-    BUILD_PACKAGES="build-essentials gcc-c++"
-    PYTHON_PACKAGES="python26 python26-devel python26-ldap python-boto"
-    PACKAGES="${BUILD_PACKAGES} ${PYTHON_PACKAGES} mysql-server mysql-devel euca2ools httpd httpd-devel erlang openssl-devel git mod_ssl yum-plugin-downloadonly bzr"
+    PACKAGES="emacs-nox"
+fi
+
 elif [ "$DISTRO" = "Ubuntu" ] || [ "$DISTRO" = "Debian" ] ; then
     # Use apt-get
     COMMAND="apt-get"
     OPTIONS="-y"
-    BUILD_PACKAGES="build-essentials gcc-c++"
-    PYTHON_PACKAGES="python26 python26-devel python26-ldap python-boto"
-    PACKAGES="${BUILD_PACKAGES} ${PYTHON_PACKAGES} mysql-server mysql-devel euca2ools httpd httpd-devel erlang openssl-devel git mod_ssl yum-plugin-downloadonly bzr"
+    PACKAGES="emacs-nox"
 fi
 
-if [ $VERBOSE ]; then
-    echo "Installing Packages."
-fi
-echo "Installing Packages."
+echo "Installing emacs."
 
 $COMMAND update -y
 $COMMAND install $OPTIONS $PACKAGES
 
+
 if [ $VERBOSE ]; then
-    echo "Packages is using $DISTRO."
-    echo "Using the $COMMAND."
-    echo "Installed $PACKAGES"
+    echo "Emacs is using $DISTRO."
+    echo "Installed emacs."
 fi
 
 exit 0;
-
-
