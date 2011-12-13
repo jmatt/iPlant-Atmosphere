d26208c87a8adb5d15932bc97e2677e278e1ed6a
esteve <esteve@iplantcollaborative.org>
2011-12-13 13:12:31 -0700

    [ATMO 2] Rabbitmq.sh can be run on Fedora/RHEL and Debian/Ubuntu thanks to the distro logic.
    	modified:   rabbitmq.sh

--- extras/install-scripts/rabbitmq.sh
+++ extras/install-scripts/rabbitmq.sh
@@ -1,1 +1,22 @@
 #!/bin/sh
+############### DISTRO CHECK ###################
+if [ -z "$DISTRO" ]; then
+	. ./distro.sh
+fi
+
+############### Install RabbitMQ ################
+if [ "$DISTRO" = "CentOS" ] || [ "$DISTRO" = "Fedora" ] ; then
+	wget http://www.rabbitmq.com/releases/rabbitmq-server/v2.7.0/rabbitmq-server-2.7.0-1.noarch.rpm
+	# Use rpm
+	COMMAND="rpm"
+	OPTIONS="-i"
+	FILE="rabbitmq-server-2.7.0-1.noarch.rpm"
+elif [ "$DISTRO" = "Ubuntu" ] || [ "$DISTRO" = "Debian" ] ; then
+	wget http://www.rabbitmq.com/releases/rabbitmq-server/v2.7.0/rabbitmq-server_2.7.0-1_all.deb
+	#Use deb
+	COMMAND="dpkg"
+	OPTIONS="-i"
+	FILE="rabbitmq-server-2.7.0-1_all.deb"
+fi
+
+$COMMAND $OPTIONS $FILE
